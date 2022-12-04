c41b9e0fc0f22b7ea3fb1be6c9d1b9d9b55900c6
Vera <vera.krutsina@gmail.com>
2022-12-03 16:09:53 -0800

    feat simple carousels

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const textArray = [
+  'Hello, Hope you are doing well!', 'Really nice day outside', 'what a beautiful morning'
+];
+
+function Carousel({items}){
+
+  const [current, setCurrent] = useState(0);
+  const isPrevDisabled = current === 0 ? true : false; 
+  const isNextDisabled = current === items.length -1 ? true : false; 
+
+  const handleNext = () => setCurrent(current + 1);
+
+  const handlePrev = () => setCurrent(current - 1);
+
+
+  return (
+     <div className='carousel container'>
+      <button onClick={handlePrev} disabled={isPrevDisabled}>{'<'}</button>
+        {items.map((item, index) => {
+          return (
+            <div key={index}>
+              {index === current && <h1>{item}</h1>}
+            </div>
+          )
+        })}
+      <button onClick={handleNext} disabled={isNextDisabled}>{'>'}</button>
+     </div>
+  )
+}
+
+
+function InfiniteCarousel({items}){
+
+  const [current, setCurrent] = useState(0);
+  const length = items.length;
+
+  const handleNext = () => {
+    current === length - 1 ?  setCurrent(0) :  setCurrent(current + 1);
+  }
+
+  const handlePrev = () => {
+    current === 0 ?  setCurrent(items.length - 1) :  setCurrent(current - 1);
+  };
+
+
+  return (
+     <div className='carousel container'>
+      <button onClick={handlePrev} >{'<'}</button>
+        {items.map((item, index) => {
+          return (
+            <div key={index}>
+              {index === current && <h1>{item}</h1>}
+            </div>
+          )
+        })}
+      <button onClick={handleNext}>{'>'}</button>
+     </div>
+  )
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Carousel</header>
+      <Carousel items={textArray} />
+      <InfiniteCarousel items={textArray} />
     </div>
   );
 }
